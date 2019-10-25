6d9af4f81220b1ad0eb45519ca15481969853dc5
Steven G. Harms <steven.harms@flatironschool.com>
2019-10-25 10:21:51 -0400

    Port routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :authors, only: [:index, :show]
+  resources :posts, only: [:index, :show]
+
+  root 'posts#index'
 end
